23581540311cae8e396f3f2db003ecb831c611e7
Tzvetan Mikov <tmikov@bloomberg.net>
2014-10-08 16:53:25 -0700

    index.js: code lazy service open and new URL structure
    For example /request/blp/refdata/HistoricalData

--- examples/ex1.sh
+++ examples/ex1.sh
@@ -6,9 +6,17 @@ HOST="http://localhost:3000"
 echo "Connecting..."
 curl -X POST $HOST/connect --data '{ "x" : 1 }'
 
-curl -X POST $HOST/request/HistoricalDataRequest --data @- <<EOF
+curl -X POST $HOST/request/blp/refdata/HistoricalData --data @- <<EOF
 { "securities": ["IBM US Equity"],
             "fields": ["PX_LAST", "OPEN", "EPS_ANNUALIZED"],
+            "startDate": "20120101",
+            "endDate": "20120301",
+            "periodicitySelection": "DAILY" }
+EOF
+
+curl -X POST $HOST/request/blp/refdata/HistoricalData --data @- <<EOF
+{ "securities": ["AAPL US Equity"],
+            "fields": ["PX_LAST", "OPEN"],
             "startDate": "20120101",
             "endDate": "20120301",
             "periodicitySelection": "DAILY" }
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 "use strict";
 
 var http = require('http');
-var url = require('url');
+//var url = require('url');
 var connect = require('connect');
 var morgan = require('morgan');
 var jsonBody = require('body/json');
-var textBody = require('body');
-var parseurl = require('parseurl');
+//var textBody = require('body');
+//var parseurl = require('parseurl');
 
 var blpapi = require('blpapi');
 
@@ -16,86 +16,85 @@ var app = connect();
 
 var serviceRefdata = 0;
 
-var state = { session:null }
+var session = {
+  blpsess:null,
+  services:{}
+};
 
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 app.use( '/connect', function (req, res, next) {
-  state.session = new blpapi.Session({ serverHost: hp.serverHost,
+  session.blpsess = new blpapi.Session({ serverHost: hp.serverHost,
 				     serverPort: hp.serverPort });
 
-  var ref = serviceRefdata++;
 
-  state.session.on('SessionStarted', function(m) {
+  session.blpsess.on('SessionStarted', function(m) {
     console.log(m);
-    state.session.openService('//blp/refdata', ref);
+    res.end("connected");
   });
 
-  state.session.on('ServiceOpened', function(m) {
-    console.log(m);
-    // Check to ensure the opened service is the refdata service
-    if (m.correlations[0].value == ref) {
-      res.end("connected");
-    }
-  });
-
-  state.session.start();
+  session.blpsess.start();
 });
 
-/*if (!String.prototype.endsWith) {
-  Object.defineProperty(String.prototype, 'endsWith', {
-    value: function (searchString, position) {
-      var subjectString = this.toString();
-      if (position === undefined || position > subjectString.length) {
-        position = subjectString.length;
-      }
-      position -= searchString.length;
-      var lastIndex = subjectString.indexOf(searchString, position);
-      return lastIndex !== -1 && lastIndex === position;
-    }
-  });
-} */
-
-function endsWith ( a, b )
-{
-  var la = a.length;
-  var lb = b.length;
-  if (lb > la)
-    return false;
-  return a.substring(la-lb) === b;
-}
-
 app.use( '/request/', function (req,res,next) {
-  var reqName = req.url.substring(1);
-  if (!endsWith(reqName, "Request")) {
-    next("Invalid request name");
+  var t = req.url.split('/');
+  var reqParts = [];
+  for ( var i = 0; i < t.length; ++i )
+    if (t[i])
+      reqParts.push(t[i]);
+  if (reqParts.length != 3) {
+    next("invalid request format");
     return;
   }
-  reqName = reqName.substring(0,reqName.length - 7);
 
-  if (!state.session) {
+  if (!session.blpsess) {
     var err = "Not connected";
     next(err);
     return;
   }
 
-  jsonBody( req, res, function (err, body) {
-    if (err) {
-      console.log("ERRROR!", err );
-      next(err);
-      return;
-    }
+  var service = "//" + reqParts[0] + "/" + reqParts[1];
+  var ref = serviceRefdata++;
+  var check = function () {
+      if (!(service in session.services)) {
+          console.log("Opening service", reqParts[0], reqParts[1]);
+          session.blpsess.openService(service, ref);
+          session.blpsess.once('ServiceOpened', function (m) {
+              console.log(m);
+              if (m.correlations[0].value == ref) {
+                  session.services[service] = "open";
+                  opened();
+              }
+          });
+      }
+      else {
+          console.log("Using cached service", reqParts[0], reqParts[1]);
+          opened();
+      }
+  };
 
-    console.log(JSON.stringify(body));
+  var opened = function () {
+    jsonBody( req, res, function (err, body) {
+      if (err) {
+        console.log("ERRROR!", err );
+        next(err);
+        return;
+      }
 
-    var ref = serviceRefdata++;
+      console.log(JSON.stringify(body));
 
-    state.session.request('//blp/refdata', reqName + "Request", body, ref );
-    state.session.once(reqName + "Response", function(m) {
-	  res.setHeader("content-type", "application/json");
-	  res.end(JSON.stringify(m));
-    })
-  });
+      var ref = serviceRefdata++;
+
+      session.blpsess.request(service, reqParts[2]+ "Request", body, ref );
+      session.blpsess.once(reqParts[2] + "Response", function(m) {
+	    res.setHeader("content-type", "application/json");
+	    res.end(JSON.stringify(m));
+      })
+    });
+  };
+
+  check();
+
 });
 
 
