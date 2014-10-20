914813e89d11c4015a5314cc35886d3d64aad612
Tzvetan Mikov <tmikov@bloomberg.net>
2014-10-20 15:47:37 -0700

    Separate version check and api-session into middleware

--- index.js
+++ index.js
@@ -7,75 +7,24 @@ var connect = require('connect');
 var morgan = require('morgan');
 var jsonBodyAsync = Promise.promisify( require('body/json') );
 var dispatch = require("dispatch");
-var parseurl = require("parseurl");
-var qs = require("qs");
-var uid = require("uid-safe");
-
 
 var BAPI = require("./lib/blpapi.js");
+
+var versionCheck = require("./lib/versioncheck.js");
+var apiSession = require("./lib/api-session.js");
 
 var app = connect();
 
 app.use(morgan('combined'));
 
-function versionCheck ( verMajor, verMinor, app ) {
-    return function ( req, res, next ) {
-        var re = /^\/v(\d+)\.(\d+)\//;
-        var match = re.exec(req.url);
-        if (match && parseInt(match[1]) == verMajor && parseInt(match[2]) >= verMinor) {
-            req.originalUrl = req.originalUrl || req.url;
-            req.url = req.url.substr( match[0].length-1); // remove the version prefix
-            return app(req, res, next);
-        }
-        else
-            return next();
-    };
-}
-
 var api1 = connect();
-api1.use( handleSession );
+api1.use( apiSession() );
 api1.use( dispatch({
     "/connect": onConnect,
     "/request/:ns/:service/:request" : onRequest
 }));
 
 app.use( versionCheck( 1, 0, api1 ) );
-
-var g_store = {};
-
-function handleSession ( req, res, next ) {
-    var parsed = parseurl(req);
-    var query = qs.parse(parsed.query);
-
-    req.parsedQuery = query;
-    res.sendResponse = function ( obj ) {
-        var p = Promise.resolve(undefined);
-        if (req.session)
-            p = Promise.resolve(req.session.sessid || uid(24)).then(function(sessid) {
-                g_store[':'+sessid] = req.session;
-                obj.sessid = sessid;
-            });
-        p.then(function(){
-            var json = JSON.stringify(obj);
-            var str;
-            if (query.callback) {
-                str = query.callback + "(" + json + ");";
-                res.setHeader("content-type", "text/javascript");
-            }
-            else {
-                str = json;
-                res.setHeader("content-type", "application/json");
-            }
-            res.end(str);
-        });
-    };
-
-    var session = query.sessid && g_store[':'+query.sessid];
-    if (session)
-        req.session = session;
-
-    next();
-}
 
 var hp = { serverHost: '127.0.0.1', serverPort: 8194 };
 
