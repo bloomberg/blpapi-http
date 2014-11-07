057c206b7f89ebeeaecd67128d4e42c1a6fb2933
Tzvetan Mikov <tmikov@bloomberg.net>
2014-11-07 10:35:25 -0800

    Handle more errors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var Promise = require("bluebird");
-var debug = require("debug")("bprox");
+var debugMod = require("debug");
 
 var http = require('http');
 var connect = require('connect');
@@ -13,6 +13,10 @@ var BAPI = require("./lib/blpapi-wrapper.js");
 
 var versionCheck = require("./lib/versioncheck.js");
 var apiSession = require("./lib/api-session.js");
+
+var debug = debugMod("bprox:debug");
+var info = debugMod("bprox:info");
+var error = debugMod("bprox:error");
 
 try {
     var conf = require('./lib/config.js');
@@ -35,17 +39,6 @@ app.use( versionCheck( 1, 0, api1 ) );
 
 var hp = { serverHost: conf.get('api.host'), serverPort: conf.get('api.port') };
 
-function sendError ( res, err, where ) {
-    var status;
-    if (err.data && err.data.reason) {
-        var r = err.data.reason;
-        status = { source:r.source, category:r.category, errorCode:r.errorCode };
-    }
-    else
-        status = {}
-    res.sendEnd( status, err.message || where, 401 );
-}
-
 function onConnect (req, res, next) {
     if (req.session && session.blpsess) {
         debug("already connected");
@@ -59,7 +52,7 @@ function onConnect (req, res, next) {
         res.sendEnd( 0, "Connected" );
     }).catch( function(err) {
         debug("error connecting:", err);
-        sendError( res, err, "Error connecting" );
+        res.sendError( err, "Error connecting" );
     });
 }
 
@@ -67,7 +60,7 @@ function onRequest (req, res, next, ns, svName, reqName) {
     var session = req.session;
 
     if (!session || !session.blpsess) {
-        return sendError( res, {}, "Not connected" );
+        return res.sendError( {}, "Not connected", {category:"NO_AUTH"} );
     }
 
     var p = req.method == "GET" ? Promise.resolve( req.parsedQuery.q ) : jsonBodyAsync( req, res );
@@ -76,7 +69,7 @@ function onRequest (req, res, next, ns, svName, reqName) {
         session.blpsess.request( "//" + ns + "/" + svName, reqName, body, function (err, data, last) {
             if (err){
                 debug("request error:", err);
-                return sendError( res, err, "Request error" );
+                return res.sendError( err, "Request error", {category:"BAD_ARGS"} );
             }
             var p = res.sendChunk( data );
             if (last) {
@@ -85,6 +78,8 @@ function onRequest (req, res, next, ns, svName, reqName) {
                 });
             }
         });
+    }).catch( function(err) {
+        return res.sendError( err, "Request error", {category:"BAD_ARGS"} );
     });
  }
 
@@ -95,3 +90,6 @@ function onRequest (req, res, next, ns, svName, reqName) {
  });
 
  server.listen(conf.get('port'));
+ server.on('listening', function() {
+     console.log('listening on', conf.get('port') );
+ });
