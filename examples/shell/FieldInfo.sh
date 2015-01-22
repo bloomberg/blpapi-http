5ca51cca4357155c487c4b173da7ae10d211784e
Chris Martin <aceofsteel2003@yahoo.com>
2015-01-22 13:56:29 -0500

    Add Json requests and modify shell commands

---
+++ examples/shell/FieldInfo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# usage: ./FieldInfo.sh <request.json>
+
+if [ "$#" -ne 1 ]; then
+	echo "Missing Field Info JSON"
+	exit 1
+fi
+
+REQUEST="$1"
+
+curl -v -X POST "https://http-api.openbloomberg.com/request/blp/apiflds/FieldInfo"  \
+    --cacert bloomberg.crt \
+    --cert   client.crt    \
+    --key    client.key    \
+    --data @"$REQUEST"
