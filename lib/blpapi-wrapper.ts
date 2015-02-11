53242e97866a8c958307e5af500a07082c6a1737
Eric N. Vander Weele <ericvw@gmail.com>
2015-02-11 12:45:18 -0800

    bump tslint: 2.1.0 => 2.1.1
    This picks up fixes addressed in palantir/tslint#292 and
    palantir/tslint#302.

--- lib/blpapi-wrapper.ts
+++ lib/blpapi-wrapper.ts
@@ -35,13 +35,11 @@ export class Subscription extends events.EventEmitter {
     }
 
     private toJSON(): Object {
-        // bug in tslint 2.1.0(issue 292). Temp disable it.
-        /* tslint:disable variables-before-functions */
         var result: { security: string; fields: string[]; options?: any; } = {
             security: this.security,
             fields: this.fields
         };
-        /* tslint:enable */
+
         if (null !== this.options) {
             result.options = this.options;
         }
@@ -188,8 +186,6 @@ export class Session extends events.EventEmitter {
         log('Session terminating');
         trace(ev);
 
-        // bug in tslint 2.1.0(issue 292). Temp disable it.
-        /* tslint:disable variables-before-functions */
         _([{prop: 'eventListeners', cleanupFn: (eventName: string): void => {
             this.session.removeAllListeners(eventName);
          }},
@@ -205,7 +201,6 @@ export class Session extends events.EventEmitter {
             });
             this[table.prop] = null;
         });
-        /* tslint:enable */
 
         if (!this.stopped) {
             this.stopped = Promise.resolve();
