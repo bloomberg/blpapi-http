0fd3924cdb8040da85a8a7944b1177db3fb929f8
Balazs Bokodi <balazs.bokodi@yahoo.co.uk>
2015-02-11 22:33:30 +0000

    load json from file add comments

--- examples/java/ReferenceData.java
+++ examples/java/ReferenceData.java
@@ -15,8 +15,13 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
 
 public class ReferenceData {
-    public static final String url =
-    "https://http-api.openbloomberg.com/request/blp/refdata/ReferenceData"; 
+    public static final String apiUrl = "https://http-api.openbloomberg.com"
+        + "/request/blp/refdata/ReferenceData";
+    public static final String keyStorePW = "secure";
+    public static final String trustStorePW = "secure2";
+    public static final String clientCert = "client.p12";
+    public static final String bbCert = "bloomberg.jks";
+
     public static void main(String[] args) {
     
         if (1 > args.length) {
@@ -25,44 +30,44 @@ public class ReferenceData {
         }
         String jsonFile = args[0];
         try {
+            // load the client public/private key from PKCS12
             KeyStore clientStore = KeyStore.getInstance("PKCS12");
-            clientStore.load(new FileInputStream("client.p12"),
-                    "secure".toCharArray());
+            clientStore.load(new FileInputStream(clientCert), keyStorePW.toCharArray());
 
-            KeyManagerFactory kmf = KeyManagerFactory
-                .getInstance(KeyManagerFactory.getDefaultAlgorithm());
-            kmf.init(clientStore, "secure".toCharArray());
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance(
+                KeyManagerFactory.getDefaultAlgorithm());
+            kmf.init(clientStore, keyStorePW.toCharArray());
             KeyManager[] kms = kmf.getKeyManagers();
 
+            // load the public key of the CA from JKS, 
+            // so we can verify the server certificate.
             KeyStore trustStore = KeyStore.getInstance("JKS");
-            trustStore.load(new FileInputStream("bloomberg.jks"),
-                    "secure2".toCharArray());
+            trustStore.load(new FileInputStream(bbCert), trustStorePW.toCharArray());
 
-            TrustManagerFactory tmf = TrustManagerFactory
-                .getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(
+                TrustManagerFactory.getDefaultAlgorithm());
             tmf.init(trustStore);
             TrustManager[] tms = tmf.getTrustManagers();
 
-            SSLContext sslContext = null;
-            sslContext = SSLContext.getInstance("TLS");
+            // initialize the SSLContext with the keys,
+            // KeyManager: client public/private key, TrustManager: server public key
+            SSLContext sslContext = SSLContext.getInstance("TLS");
             sslContext.init(kms, tms, new SecureRandom());
 
-            HttpsURLConnection.setDefaultSSLSocketFactory(sslContext
-                    .getSocketFactory());
-            URL url = new URL(
-                    "https://localhost/index.html");
-
-            HttpsURLConnection urlConn = (HttpsURLConnection) url
-                .openConnection();
+            HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+            URL url = new URL(apiUrl);
+            
+            // open connection to the server
+            HttpsURLConnection urlConn = (HttpsURLConnection) url.openConnection();
 
             urlConn.setRequestMethod("POST");
-            urlConn.setRequestProperty("User-Agent", "noname");
+            urlConn.setRequestProperty("User-Agent", "blpapi-http-java-example");
             urlConn.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
             urlConn.setDoOutput(true);
-            urlConn.setRequestProperty("Content-Type",
-                    "application/json; charset=utf8");
-            DataOutputStream wr = new DataOutputStream(
-                    urlConn.getOutputStream());
+            urlConn.setRequestProperty("Content-Type", "application/json; charset=utf8");
+
+            // write the json request to the output stream
+            DataOutputStream wr = new DataOutputStream(urlConn.getOutputStream());
             FileInputStream fis = new FileInputStream(jsonFile);
             byte[] buffer = new byte[1024];
             int len = fis.read(buffer);
@@ -73,12 +78,12 @@ public class ReferenceData {
             wr.flush();
             wr.close();
 
+            // read the whatever we get back
             int responseCode = urlConn.getResponseCode();
             System.out.println("\nSending 'POST' request to URL : " + url);
             System.out.println("Response Code : " + responseCode);
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                        urlConn.getInputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
             String inputLine;
             StringBuffer response = new StringBuffer();
 
@@ -91,7 +96,5 @@ public class ReferenceData {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
-
 }
